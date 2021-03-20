3ebc96894adef12b8b7894b9ed061d1bb93e5f25
Adam O'Neil <adamosoftware@gmail.com>
2021-03-19 20:07:42 -0400

    Add project files.

---
+++ RadzenGridHelper/frmMain.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace RadzenGridHelper
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.tbAssembly = new System.Windows.Forms.ToolStripTextBox();
+            this.btnSelectAssembly = new System.Windows.Forms.ToolStripButton();
+            this.tbContextVar = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tbItemType = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.dgvColumns = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.colPropertyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEditorControl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColumns)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel3,
+            this.tbAssembly,
+            this.btnSelectAssembly,
+            this.tbContextVar,
+            this.toolStripLabel2,
+            this.tbItemType,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(664, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(61, 22);
+            this.toolStripLabel3.Text = "Assembly:";
+            // 
+            // tbAssembly
+            // 
+            this.tbAssembly.Name = "tbAssembly";
+            this.tbAssembly.Size = new System.Drawing.Size(100, 25);
+            // 
+            // btnSelectAssembly
+            // 
+            this.btnSelectAssembly.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnSelectAssembly.Image = ((System.Drawing.Image)(resources.GetObject("btnSelectAssembly.Image")));
+            this.btnSelectAssembly.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSelectAssembly.Name = "btnSelectAssembly";
+            this.btnSelectAssembly.Size = new System.Drawing.Size(51, 22);
+            this.btnSelectAssembly.Text = "Select...";
+            // 
+            // tbContextVar
+            // 
+            this.tbContextVar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.tbContextVar.Name = "tbContextVar";
+            this.tbContextVar.Size = new System.Drawing.Size(50, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(96, 22);
+            this.toolStripLabel2.Text = "Context Variable:";
+            // 
+            // tbItemType
+            // 
+            this.tbItemType.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.tbItemType.Name = "tbItemType";
+            this.tbItemType.Size = new System.Drawing.Size(200, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(40, 22);
+            this.toolStripLabel1.Text = "TItem:";
+            // 
+            // dgvColumns
+            // 
+            this.dgvColumns.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvColumns.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colPropertyName,
+            this.colTitle,
+            this.colEditorControl});
+            this.dgvColumns.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvColumns.Location = new System.Drawing.Point(0, 25);
+            this.dgvColumns.Name = "dgvColumns";
+            this.dgvColumns.RowTemplate.Height = 25;
+            this.dgvColumns.Size = new System.Drawing.Size(664, 240);
+            this.dgvColumns.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 265);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(664, 66);
+            this.panel1.TabIndex = 2;
+            // 
+            // colPropertyName
+            // 
+            this.colPropertyName.HeaderText = "Property Name";
+            this.colPropertyName.Name = "colPropertyName";
+            // 
+            // colTitle
+            // 
+            this.colTitle.HeaderText = "Title";
+            this.colTitle.Name = "colTitle";
+            // 
+            // colEditorControl
+            // 
+            this.colEditorControl.HeaderText = "Editor Control";
+            this.colEditorControl.Name = "colEditorControl";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 331);
+            this.Controls.Add(this.dgvColumns);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Radzen Blazor Grid Helper";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvColumns)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox tbItemType;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox tbContextVar;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripTextBox tbAssembly;
+        private System.Windows.Forms.ToolStripButton btnSelectAssembly;
+        private System.Windows.Forms.DataGridView dgvColumns;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPropertyName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEditorControl;
+    }
+}
+
